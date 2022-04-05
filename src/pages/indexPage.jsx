7737cbfcb5d9cd73478990ad6384356808ff9913
Anurag kumar <anurag.kkumarr@gmail.com>
2022-04-06 03:57:33 +0530

    Added Routing and Homepage

--- src/pages/indexPage.jsx
+++
@@ -1,7 +0,0 @@
-export {Homepage} from './homepage/Homepage';
-export {ProductListing} from './productListingPage/ProductListing';
-export {Login} from './loginPage/Login';
-export {Logout} from './logoutPage/Logout';
-export {CartPage} from './cartPage/CartPage';
-export {Signup} from './signupPage/Signup';
-export {WishList} from './wishListPage/WishList';
